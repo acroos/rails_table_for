554119b4e49801dde88554415760d53ebe8ca66d
Austin Roos <a.roos@sig.eu>
2020-03-20 19:57:06 +0100

    Initial commit

---
+++ lib/rails_table_for.rb
@@ -0,0 +1,6 @@
+require "rails_table_for/version"
+
+module RailsTableFor
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/rails_table_for/version.rb
@@ -0,0 +1,3 @@
+module RailsTableFor
+  VERSION = "0.1.0"
+end
