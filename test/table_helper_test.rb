985e5fe4de35a954d416652bb3f23ae536a3b200
Austin Roos <a.roos@sig.eu>
2020-03-21 10:58:00 +0100

    Improve testing by storing expected html in files instead of strings

--- test/table_helper_test.rb
+++ test/table_helper_test.rb
@@ -1,15 +1,15 @@
 require 'byebug'
 require 'action_view'
 require 'minitest/autorun'
-require_relative '../lib/rails_table_for/table_helper'
+require 'rails_table_for/table_helper'
 
 class TestTableHelper < Minitest::Test
   include TableHelper, ActionView::Context
 
   def test_empty_table
     @records = []
-    html = table_for(@records)
-    assert_equal '<table></table>', html
+    html = table_for @records
+    test_html_output(html)
   end
 
   def test_simplest_table
@@ -18,7 +18,25 @@ class TestTableHelper < Minitest::Test
       2.times { record.expect :primary_key, 'id' }
       record.expect :id, '1'
     end
-    html = table_for(@records)
-    assert_equal '<table><thead><tr><th>id</th></tr></thead><tbody><tr><td>1</td></tr></tbody></table>', html
+    html = table_for @records
+    test_html_output(html)
+  end
+
+  private
+  def test_html_output(actual_html)
+    test_name = caller_locations[0].label
+    actual_html = remove_all_whitespace(actual_html)
+    expected_html = fetch_test_html(test_name)
+    assert_equal expected_html, actual_html
+  end
+
+  def fetch_test_html(test_name)
+    path = File.join(__dir__, 'data', "#{test_name}.html")
+    formatted_html = File.read(path)
+    remove_all_whitespace(formatted_html)
+  end
+
+  def remove_all_whitespace(text)
+    text.gsub(/\s/, '')
   end
 end
